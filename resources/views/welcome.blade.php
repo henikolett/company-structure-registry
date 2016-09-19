35ec975601e544e3f514909448e8b18527f16cfc
Nikolett Hegedüs <henikolett@gmail.com>
2016-09-19 08:25:07 +0200

    Welcome page update

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -2,8 +2,10 @@
 
 @section('content')
 
-            <h1 class="header">
-                Company Structure Registry
-            </h1>
+    <h1 class="header">Company Structure Registry</h1>
+
+    <div>
+        <a class="btn" href="/elements">Start here <i class="material-icons right">send</i></a>
+    </div>
 
     @endsection
